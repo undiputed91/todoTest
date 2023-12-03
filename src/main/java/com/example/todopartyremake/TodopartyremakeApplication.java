ccf782f427801ff02f6b0e80c0f5be259ddf69c8
seunghosin <shin2896@naver.com>
2023-12-03 21:59:55 +0900

    init git

---
+++ src/main/java/com/example/todopartyremake/TodopartyremakeApplication.java
@@ -0,0 +1,13 @@
+package com.example.todopartyremake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodopartyremakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodopartyremakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todopartyremake/TodopartyremakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todopartyremake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodopartyremakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
